3f8030508ce3b2bdc1334b5af47af0a815bef19f
Nolhan <nolhan.dev@gmail.com>
2024-02-10 15:04:35 +0100

    Updated /api/tables/[slug] route to remove fs (not compatible with production build)

--- src/app/api/tables/[slug]/route.ts
+++ src/app/api/tables/[slug]/route.ts
@@ -1,21 +1,15 @@
 import csv2json from "@/utils/csv2json";
-import {promises as fs} from "fs";
-import {redirect} from "next/navigation";
 import {NextRequest} from "next/server";
-import iconv from "iconv-lite"
 
 export async function GET(request: NextRequest, context: { params: { slug: string } }) {
   const { slug } = context.params
 
-  const csv = await fs.readFile(process.cwd() + `/public/tables/${slug}.csv`).catch(() => null)
+  const res = await fetch(`${process.env.APP_URL}/tables/${slug}.csv`).catch(null)
 
-  if (!csv) return new Response(null, { status: 404, statusText: "File not found" })
+  if (!res || res.status !== 200) return new Response(null, { status: 404, statusText: "File not found" })
 
-  const utf8Data = iconv.decode(csv, 'ISO-8859-1');
-
-  console.log(utf8Data)
-
-  const json = csv2json(utf8Data, ["city", "lat", "lng"], ",")
+  const csv = await res.text()
+  const json = csv2json(csv, ["city", "lat", "lng"], ",")
 
   return Response.json(json, { status: 200, statusText: "OK" })
 }
