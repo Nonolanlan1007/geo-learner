7e9e25a80c8892f1e510508696086454566fe2aa
Nolhan <nolhan.dev@gmail.com>
2024-02-10 15:18:43 +0100

    Fixed encode errors

--- src/app/api/tables/[slug]/route.ts
+++ src/app/api/tables/[slug]/route.ts
@@ -8,7 +8,8 @@ export async function GET(request: NextRequest, context: { params: { slug: strin
 
   if (!res || res.status !== 200) return new Response(null, { status: 404, statusText: "File not found" })
 
-  const csv = await res.text()
+  const buffer = await res.arrayBuffer()
+  const csv = new TextDecoder("ascii").decode(buffer)
   const json = csv2json(csv, ["city", "lat", "lng"], ",")
 
   return Response.json(json, { status: 200, statusText: "OK" })
